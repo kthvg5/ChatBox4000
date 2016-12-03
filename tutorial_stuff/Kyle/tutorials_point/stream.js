a569ee22bc60d9f80035591bbb5fc9a0547a27f8
kthvg5 <kthvg5@mst.edu>
2016-12-03 16:45:52 -0600

    Added Thanks Gary folder containing his initial site taht we will edit. Let's do this guys!

--- tutorial_stuff/Kyle/tutorials_point/stream.js
+++ tutorial_stuff/Kyle/tutorials_point/stream.js
@@ -1,3 +1,4 @@
+/*
 var fs = require("fs");
 var data = '';
 
@@ -21,3 +22,71 @@ readerStream.on('error', function(err){
 });
 
 console.log("program Ended");
+*/
+
+
+
+/*
+var fs = require("fs");
+var data = 'Simply Easy Learning';
+
+// Creat a writable stream
+var writerStream = fs.createWriteStream('output.txt');
+
+// Write the data to stream with encoding to be utf8
+writerStream.write(data, 'UTF8');
+
+// Mark the end of the file
+writerStream.end();
+
+// Handle stream events --> finish, and error
+writerStream.on('finish', function(){
+	console.log("Write completed.");
+});
+
+writerStream.on('error', function(err){
+	console.log(err.stack);
+});
+
+console.log("Program Ended");
+*/
+
+
+/*
+var fs = require("fs");
+
+// Create a readable stream
+var readerStream = fs.createReadStream('input.txt');
+
+// Create a writable stream
+var writerStream = fs.createWriteStream('output.txt');
+
+// Pipe the read and write operations
+// read input.txt and write data to output.txt
+
+readerStream.pipe(writerStream);
+
+console.log("Program Ended")
+*/
+
+/*
+var fs = require("fs");
+var zlib = require('zlib');
+
+// Compress the file input.txt to input.txt.gz
+fs.createReadStream('input.txt')
+	.pipe(zlib.createGzip())
+	.pipe(fs.createWriteStream('input.txt.gz'));
+
+console.log("File Compressed.");
+*/
+
+var fs = require("fs");
+var zlib = require('zlib');
+
+// Decompress the file input.txt.gx to input.txt
+fs.createReadStream('input.txt.gz')
+	.pipe(zlib.createGunzip())
+	.pipe(fs.createWriteStream('input.txt'));
+
+console.log("File Decompressed.");
