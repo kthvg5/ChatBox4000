649b2917b255414ac46c4a22d0140496c7b6828b
mcmhd3 <mcmhd3@mst.edu>
2016-12-07 18:44:47 -0600

    Add files via upload
    This is nowhere near done, but I wanted to get something put up here. I am going to be working on it later tonight, but right now I have to get engineering statistics done for tomorrow.

---
+++ Thanks Gary /chatroom/admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="conn.php">
+  Session Search:<br>
+  <input type="text" name="SessionID">
+  <input type="submit" value="Submit">
+</form>
+<form action="conn.php">
+  User Search:<br>
+  <input type="text" name="UserID">
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
